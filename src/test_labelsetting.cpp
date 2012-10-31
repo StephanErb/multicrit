c9561fc61a2987eb901db504c9cb517dbcbdc122
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2012-10-31 12:41:28 +0000

    Simple static grid test.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@15 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/test_labelsetting.cpp
+++ src/test_labelsetting.cpp
@@ -15,13 +15,24 @@
 #include "LabelSettingAlgorithm.hpp"
 #include "GraphGenerator.hpp"
 
-
 typedef utility::datastructure::DirectedIntegerBiWeightedEdge Edge;
 typedef utility::datastructure::KGraph<Edge> Graph;
 typedef utility::datastructure::NodeID NodeID;
-typedef Edge::edge_data Weight;
+typedef Edge::edge_data Label;
 
-void createGrid(Graph& graph) {
+
+void assertTrue(bool cond, std::string msg) {
+	if (!cond) {
+		std::cout << "FAILED: " << msg << std::endl;
+		exit(-1);
+	}
+}
+
+bool contains(LabelSettingAlgorithm<Graph>& algo, const NodeID node, const Label label) {
+	return std::find(algo.begin(node), algo.end(node), label) != algo.end(node);
+}
+
+void createGridSimple(Graph& graph) {
 	graph.addNode();
 	const NodeID START = NodeID(0);
 
@@ -29,24 +40,36 @@ void createGrid(Graph& graph) {
 	const NodeID END = NodeID(1);
 
 	graph.addNode(); // non-dominated path from start to end
-	graph.addEdge(START, Edge(NodeID(2), Weight(1,2)));
-	graph.addEdge(NodeID(2), Edge(END, Weight(1,1)));
+	graph.addEdge(START, Edge(NodeID(2), Label(1,2)));
+	graph.addEdge(NodeID(2), Edge(END, Label(1,1)));
 
 	graph.addNode(); // non-dominated path from start to end
-	graph.addEdge(START, Edge(NodeID(3), Weight(2,1)));
-	graph.addEdge(NodeID(3), Edge(END, Weight(1,1)));
+	graph.addEdge(START, Edge(NodeID(3), Label(2,1)));
+	graph.addEdge(NodeID(3), Edge(END, Label(1,1)));
 
 	graph.addNode(); // dominated path from start to end
-	graph.addEdge(START, Edge(NodeID(4), Weight(1,1)));
-	graph.addEdge(NodeID(4), Edge(END, Weight(4,4)));
+	graph.addEdge(START, Edge(NodeID(4), Label(1,1)));
+	graph.addEdge(NodeID(4), Edge(END, Label(4,4)));
 
 	graph.finalize();
 }
+
+void testGridSimple() {
+	Graph graph;
+	createGridSimple(graph);
+
+	LabelSettingAlgorithm<Graph> algo(graph);
+	algo.run();
+
+	assertTrue(contains(algo, NodeID(1), Label(2,3)), "");
+	assertTrue(contains(algo, NodeID(1), Label(3,2)), "");
+}
  
 int main() {
+	testGridSimple();
+
 	GraphGenerator<Graph> generator;
 	Graph graph = generator.generateRandomGridGraph(3,3);
-	createGrid(graph);
 
 	LabelSettingAlgorithm<Graph> algo(graph);
 	algo.run();
@@ -58,9 +81,8 @@ int main() {
 		}
 		std::cout << std::endl;
 	}
+	
 	std::cout << "Tests passed successfully." << std::endl;
-
-
 	return 0;
 }
 
