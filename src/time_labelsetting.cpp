00b08fbdd583d77c56dd5577d58539c0f53c2118
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2012-11-04 23:25:10 +0000

    Reorganize the benchmarking code.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@31 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/time_labelsetting.cpp
+++ src/time_labelsetting.cpp
@@ -19,105 +19,58 @@ typedef utility::datastructure::KGraph<Edge> Graph;
 typedef utility::datastructure::NodeID NodeID;
 typedef Edge::edge_data Label;
 
- 
+
+void benchmark(Graph& graph, NodeID start, NodeID target) {
+	LabelSettingAlgorithm<Graph> algo(graph);
+
+	utility::tool::TimeOfDayTimer timer;
+	timer.start();
+	algo.run(start);
+	timer.stop();
+
+	NodeID node = NodeID(graph.numberOfNodes()-1);
+	std::cout << "  Target node label count: " << algo.size(node) << std::endl;
+
+	std::cout << "  " << timer.getTimeInSeconds()  << " [s]" << std::endl << std::endl;
+}
+
 void timeGrid() {
+	std::cout << "Raith & Ehrgott Grid (hard): " << std::endl;
 	Graph graph;
 	GraphGenerator<Graph> generator;
 	generator.generateRandomGridGraph(graph, 200, 200);
-	LabelSettingAlgorithm<Graph> algo(graph);
-
-	utility::tool::TimeOfDayTimer timer;
-	timer.start();
-
-	algo.run(NodeID(0));
-
-	timer.stop();
-	std::cout << timer.getTimeInSeconds()  << " [s]" << std::endl;
-
-	NodeID node = NodeID(1);
-	std::cout << "Label count (target): " << algo.size(node) << std::endl;
+	benchmark(graph, NodeID(0), NodeID(1));
 }
 
-void timeCorrelatedGrid() {
+void timeCorrelatedGrid1() {
+	std::cout << "Machuca Grid Correlated (simple): " << std::endl;
+	Graph graph;
+	GraphGenerator<Graph> generator;
+	generator.generateRandomGridGraphWithCostCorrleation(graph, 200, 200, 0.4);
+	benchmark(graph, NodeID(0), NodeID(graph.numberOfNodes()-1));
+}
+
+void timeCorrelatedGrid2() {
+	std::cout << "Machuca Grid Correlated (difficult): " << std::endl;
 	Graph graph;
 	GraphGenerator<Graph> generator;
 	generator.generateRandomGridGraphWithCostCorrleation(graph, 200, 200, -0.4);
-	LabelSettingAlgorithm<Graph> algo(graph);
-
-	utility::tool::TimeOfDayTimer timer;
-	timer.start();
-
-	algo.run(NodeID(0));
-
-	timer.stop();
-	std::cout << timer.getTimeInSeconds()  << " [s]" << std::endl;
-
-	NodeID node = NodeID(graph.numberOfNodes()-1);
-	std::cout << "Label count (target): " << algo.size(node) << std::endl;
+	benchmark(graph, NodeID(0), NodeID(graph.numberOfNodes()-1));
 }
 
 void timeExponentialGraph() {
+	std::cout << "Exponential Graph: " << std::endl;
 	Graph graph;
 	GraphGenerator<Graph> generator;
-	generator.generateExponentialGraph(graph, 22);
-	LabelSettingAlgorithm<Graph> algo(graph);
-
-	utility::tool::TimeOfDayTimer timer;
-	timer.start();
-
-	algo.run(NodeID(0));
-
-	timer.stop();
-	std::cout << timer.getTimeInSeconds()  << " [s]" << std::endl;
-
-	NodeID node = NodeID(graph.numberOfNodes()-1);
-	std::cout << "Label count (target): " << algo.size(node) << std::endl;
+	generator.generateExponentialGraph(graph, 20);
+	benchmark(graph, NodeID(0), NodeID(graph.numberOfNodes()-1));
 }
 
 int main(int argc, char ** args) {
+	timeExponentialGraph();
 	timeGrid();
-	timeCorrelatedGrid();
-
-/*	std::ifstream ksg_input_stream;
-
-	int c;
-	while( (c = getopt( argc, args, "g:") ) != -1  ){
-		switch(c){
-		case 'g':
-			ksg_input_stream.open( optarg, std::ios::in | std::ios::binary );
-			std::cout << "Opening: " << optarg << std::endl;
-			break;
-		}
-	}
-
-	Graph graph;
-
-	std::cout << "Loading ksg..." << std::flush;
-	graph.deserialize( ksg_input_stream );
-	std::cout << "done." << std::endl;
-	ksg_input_stream.close();
-
-	graph.checkIntegrity();
-	
-*/
-	/*GraphGenerator<Graph> generator;
-	//generator.printGraph(graph);
-
-	std::cout << "Algo started" << std::endl;
-	LabelSettingAlgorithm<Graph> algo(graph);
-
-	utility::tool::TimeOfDayTimer timer;
-	timer.start();
-
-	algo.run(NodeID( 100));
-
-	timer.stop();
-	std::cout << timer.getTimeInSeconds()  << " [s]" << std::endl;
-
-	NodeID node = NodeID(graph.numberOfNodes()-1);
-	std::cout << "Label count (target): " << algo.size(node) << std::endl;
-	*/
-	
+	timeCorrelatedGrid1();
+	timeCorrelatedGrid2();
 	return 0;
 }
 
