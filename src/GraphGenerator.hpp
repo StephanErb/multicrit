ec865ee9b9366025916089669c442bc8fc727dc8
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2012-11-04 23:25:06 +0000

    Implement random grid with correlation configuration.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@27 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/GraphGenerator.hpp
+++ src/GraphGenerator.hpp
@@ -14,6 +14,7 @@
 #include <boost/random/uniform_int_distribution.hpp>
 
 #include <iostream>
+#include <cmath>
 
 template<typename graph_slot>
 class GraphGenerator {
@@ -22,13 +23,29 @@ private:
 	typedef typename graph_slot::EdgeID EdgeID;
 	typedef typename graph_slot::Edge Edge;
 	typedef typename Edge::edge_data Weight;
+	typedef typename Weight::weight_type WeightType;
 
 	// Mersenne Twister random number genrator
 	boost::random::mt19937 gen;
 
-	Weight randomWeight(int lastInRange) {
-		boost::random::uniform_int_distribution<> dist(1, lastInRange);
+	Weight randomWeight(WeightType max_cost) {
+		boost::random::uniform_int_distribution<> dist(1, max_cost);
     	return Weight(dist(gen), dist(gen));
+	}
+
+	Weight correlatedRandomWeight(WeightType max_cost, double p) {
+		boost::random::uniform_int_distribution<> dist(1, max_cost);
+
+		double first_weight = dist(gen);
+		double second_weight = dist(gen);
+
+		if (p < 0) {
+			p = p * -1;
+			second_weight = 1 + max_cost - (p * first_weight + (1-p) * second_weight);
+		} else {
+			second_weight = (p * first_weight + (1-p) * second_weight);
+		}
+    	return Weight(first_weight, second_weight);
 	}
 
 public:
@@ -39,7 +56,7 @@ public:
 	 * Generate grid graphs as used by [Raith, Ehrgott 2009]
 	 */
 	void generateRandomGridGraph(Graph& graph, int height, int width) {
-		const int maxCostRange = 10;
+		const WeightType max_cost = 10;
 		NodeID nodes[width][height];
 
 		graph.addNode();
@@ -48,7 +65,7 @@ public:
 		graph.addNode();
 		const NodeID END = NodeID(1);
 
-		// Great grid of nodes
+		// Create grid of nodes
 		int node_count = 2;
 		for (int i=0; i<width; ++i) {
 			for (int j=0; j<height; ++j) {
@@ -56,32 +73,62 @@ public:
 				nodes[i][j] = NodeID(node_count++);
 			}	
 		}
-
 		// Link START node to the left column
 		for (int j=0; j<height; ++j) {		
-			graph.addEdge(START, Edge(nodes[0][j], randomWeight(maxCostRange))); 
+			graph.addEdge(START, Edge(nodes[0][j], randomWeight(max_cost))); 
 		}
-
 		// Link right column to the END node
 		for (int j=0; j<height; ++j) {		
-			graph.addEdge(nodes[width-1][j], Edge(END, randomWeight(maxCostRange))); 
+			graph.addEdge(nodes[width-1][j], Edge(END, randomWeight(max_cost))); 
 		}
-
 		// Link adjacent nodes
 		for (int i=0; i<width; ++i) {
 			for (int j=0; j<height; ++j) {
 				NodeID current = nodes[i][j];
 				
-				if (i+1 < width)  graph.addEdge(current, Edge(nodes[i+1][j], randomWeight(maxCostRange))); 
-				if (i-1 >= 0)     graph.addEdge(current, Edge(nodes[i-1][j], randomWeight(maxCostRange))); 
-				if (j+1 < height) graph.addEdge(current, Edge(nodes[i][j+1], randomWeight(maxCostRange))); 
-				if (j-1 >= 0)     graph.addEdge(current, Edge(nodes[i][j-1], randomWeight(maxCostRange))); 
+				if (i+1 < width)  graph.addEdge(current, Edge(nodes[i+1][j], randomWeight(max_cost))); 
+				if (i-1 >= 0)     graph.addEdge(current, Edge(nodes[i-1][j], randomWeight(max_cost))); 
+				if (j+1 < height) graph.addEdge(current, Edge(nodes[i][j+1], randomWeight(max_cost))); 
+				if (j-1 >= 0)     graph.addEdge(current, Edge(nodes[i][j-1], randomWeight(max_cost))); 
 			}	
 		}
 		graph.finalize();
 		//printGraph(graph);
 	}
 
+	/** See thesis "An Analysis of Some Algorithms and Heuristics for Multiobjective Graph Search":
+	  *
+	  * Grid graph (without external start and and node) where the correlation between both costs
+	  * is configured by a mutliplier -1 <= p <=1
+
+	  * Interesting p values from easy to difficult: 0.8, 0.4, 0, -0.4, -0.8
+	  */
+	void generateRandomGridGraphWithCostCorrleation(Graph& graph, int height, int width, double p) {
+		const WeightType max_cost = 10;
+		NodeID nodes[width][height];
+
+		// Create grid of nodes
+		int node_count = 0;
+		for (int i=0; i<width; ++i) {
+			for (int j=0; j<height; ++j) {
+				graph.addNode();
+				nodes[i][j] = NodeID(node_count++);
+			}	
+		}
+		// Link adjacent nodes
+		for (int i=0; i<width; ++i) {
+			for (int j=0; j<height; ++j) {
+				NodeID current = nodes[i][j];
+				
+				if (i+1 < width)  graph.addEdge(current, Edge(nodes[i+1][j], correlatedRandomWeight(max_cost, p))); 
+				if (i-1 >= 0)     graph.addEdge(current, Edge(nodes[i-1][j], correlatedRandomWeight(max_cost, p))); 
+				if (j+1 < height) graph.addEdge(current, Edge(nodes[i][j+1], correlatedRandomWeight(max_cost, p))); 
+				if (j-1 >= 0)     graph.addEdge(current, Edge(nodes[i][j-1], correlatedRandomWeight(max_cost, p))); 
+			}	
+		}
+		graph.finalize();
+		//printGraph(graph);
+	}
 
 	/**
 	 * Simple graph with exponential number of labels. Idea taken from the paper 
@@ -113,7 +160,6 @@ public:
 		graph.finalize();
 		//printGraph(graph);
 	}
-
 
 	void printGraph(Graph& graph) {
 		FORALL_NODES(graph, node) {
