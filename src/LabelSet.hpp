abaee277a1d6afa41886024d771d2dffabbe2500
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2012-11-06 23:11:22 +0000

    Implement SplittedNaiveLabelSet.
    Tentative and permanent labels will be held in different sets.
    
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@42 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/LabelSet.hpp
+++ src/LabelSet.hpp
@@ -27,7 +27,7 @@ public:
 	typedef unsigned int Priority;
 
 	static Priority computePriority(const label_type_slot& label) {
-		return label.first_weight  + label.second_weight;
+		return label.first_weight + label.second_weight;
 	}
 
 #ifdef TREE_SET
@@ -56,16 +56,14 @@ protected:
 	/** First label where the x-coord is truly smaller */
 	static iterator x_predecessor(Set& labels, const label_type_slot& new_label) {
 #ifdef TREE_SET
-		iterator iter = labels.lower_bound(new_label);
-		--iter;
-		return iter;
+		return --labels.lower_bound(new_label);
 #else
-		return std::lower_bound(labels.begin(), labels.end(), new_label, firstWeightLess)-1;
+		return --std::lower_bound(labels.begin(), labels.end(), new_label, firstWeightLess);
 #endif	
 	}
 
 	/** First label where the y-coord is truly smaller */
-	static iterator y_predecessor(const iterator begin, const iterator end, const label_type_slot& new_label) {
+	static iterator y_predecessor(const iterator begin, const label_type_slot& new_label) {
 		iterator i = begin;
 		while (secondWeightGreaterOrEquals(*i, new_label)) {
 			++i;
@@ -100,6 +98,26 @@ protected:
 		}
 		return false;
 	}
+
+	static void insertAndRemoveDominated(Set &labels, const label_type_slot& new_label, iterator iter) {
+		iterator first_nondominated = y_predecessor(iter, new_label);
+
+#ifdef TREE_SET
+		labels.erase(iter--, first_nondominated);
+		labels.insert(iter, new_label);
+#else
+		if (iter == first_nondominated) {
+			// delete range is empty, so just insert
+			labels.insert(first_nondominated, new_label);
+		} else {
+			// replace first dominated label and remove the rest
+			*iter = new_label;
+			labels.erase(++iter, first_nondominated);
+		}
+#endif
+	}
+	
+
 };
 
 
@@ -159,7 +177,6 @@ public:
 		best_label = *labels.begin(); // one of the sentinals
 		best_label_priority = B::computePriority(best_label);
 
-
 		perm_label_counter = 2; // sentinals
 	}
 
@@ -170,33 +187,17 @@ public:
 		if (B::isDominated(labels, new_label, iter)) {
 			return false;
 		}
-		typename B::iterator first_nondominated = B::y_predecessor(iter, labels.end(), new_label);
-
-#ifdef TREE_SET
-		labels.erase(iter, first_nondominated);
-		labels.insert(new_label);
-#else
-		if (iter == first_nondominated) {
-			// delete range is empty, so just insert
-			labels.insert(new_label);
-		} else {
-			// replace first dominated label and remove the rest
-			*iter = new_label;
-			labels.erase(++iter, first_nondominated);
-		}
-#endif
+		B::insertAndRemoveDominated(labels, new_label, iter);
 		updateBestLabelIfNeeded(new_label);
 		return true;
 	}
 
-	// FIXME: The following implementations is very naive and thus horribly slow
 	void markBestLabelAsPermantent() {
 		label_type old_best_label = best_label;
 		best_label_priority = B::computePriority(*labels.begin()); // prio of sentinal
 
 		// Scan all labels: Find new best label and mark the old one as permanent.
 		for (typename B::iterator i = labels.begin(); i != labels.end(); ++i) {
-
 			if (i->permanent) {
 				continue;
 			}
@@ -229,6 +230,119 @@ public:
 	typename B::iterator end() { return labels.end(); }
 	typename B::const_iterator end() const { return labels.end(); }
 };
+
+
+
+/**
+  * Similar to the naive implementation, but temporary labels and 
+  * permanent labels are kept in different sets, limiting the set
+  * of elements that have to be scanned constanly.
+  */
+template<typename label_type_slot>
+class SplittedNaiveLabelSet : public LabelSetBase<label_type_slot > {
+public: 
+	typedef label_type_slot label_type;
+
+protected:
+	typedef LabelSetBase<label_type> B;
+
+	// sorted by increasing x (first_weight) and thus decreasing y (second_weight)
+	typename B::Set perm_labels;
+	typename B::Set temp_labels;
+
+	label_type best_label;
+	typename B::Priority best_label_priority; 
+
+	void updateBestLabelIfNeeded(const label_type& label) {
+		typename B::Priority priority = B::computePriority(label);
+		if (priority < best_label_priority) {
+			best_label_priority = priority;
+			best_label = label;
+		}
+	}
+
+public:
+	SplittedNaiveLabelSet() {
+		const typename label_type::weight_type min = std::numeric_limits<typename label_type::weight_type>::min();
+		const typename label_type::weight_type max = std::numeric_limits<typename label_type::weight_type>::max();
+
+		// add sentinals
+		perm_labels.insert(perm_labels.begin(), label_type(min, max));
+		perm_labels.insert(perm_labels.end(), label_type(max, min));
+
+		temp_labels.insert(temp_labels.begin(), label_type(min, max));
+		temp_labels.insert(temp_labels.end(), label_type(max, min));
+
+		best_label = *temp_labels.begin(); // one of the sentinals
+		best_label_priority = B::computePriority(best_label);
+	}
+
+	// Return true if the new_label is non-dominated and has been added
+	// as a temporary label to this label set. 
+	bool add(const label_type& new_label) {
+		typename B::iterator iter;
+		if (B::isDominated(temp_labels, new_label, iter)) {
+			return false;
+		}
+		typename B::iterator unused;
+		if (B::isDominated(perm_labels, new_label, unused)) {
+			return false;
+		}
+		B::insertAndRemoveDominated(temp_labels, new_label, iter);
+		updateBestLabelIfNeeded(new_label);
+		return true;
+	}
+
+	void markBestLabelAsPermantent() {
+		if (!hasTemporaryLabels()) {
+			return;
+		}
+		best_label_priority = B::computePriority(*temp_labels.begin()); // prio of sentinal
+		const label_type old_best_label = best_label;
+		typename B::iterator i = temp_labels.begin();
+
+#ifdef TREE_SET
+		perm_labels.insert(best_label);
+		while (i != temp_labels.end()) {
+			if (i->first_weight == old_best_label.first_weight && i->second_weight == old_best_label.second_weight) {
+				temp_labels.erase(i--);
+			}
+			updateBestLabelIfNeeded(*i);
+			++i;
+		}
+#else
+		perm_labels.insert(std::lower_bound(perm_labels.begin(), perm_labels.end(), best_label, B::firstWeightLess), best_label);
+		while (i != temp_labels.end()) {
+			if (i->first_weight == old_best_label.first_weight && i->second_weight == old_best_label.second_weight) {
+				i = temp_labels.erase(i);
+			}
+			updateBestLabelIfNeeded(*i);
+			++i;
+		}	
+#endif 
+	}
+
+	bool hasTemporaryLabels() const {
+		return temp_labels.size() > 2;
+	}
+
+	typename B::Priority getPriorityOfBestTemporaryLabel() const {
+		return best_label_priority;
+	}
+
+	label_type getBestTemporaryLabel() const {
+		return best_label;
+	}
+
+	/* Subtraction used to hide the sentinals */
+	std::size_t size() const { return perm_labels.size() + temp_labels.size()-4; }
+
+	typename B::iterator begin() { return perm_labels.begin(); }
+	typename B::const_iterator begin() const { return perm_labels.begin(); }
+	typename B::iterator end() { return perm_labels.end(); }
+	typename B::const_iterator end() const { return perm_labels.end(); }
+};
+
 
 
 template<typename label_type_slot>
