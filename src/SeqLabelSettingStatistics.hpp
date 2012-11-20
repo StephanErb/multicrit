a637488c0668e2732bfec436fa6dd1f68c7be570
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2012-11-20 17:22:06 +0000

    Remove useless depletion counter.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@80 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/SeqLabelSettingStatistics.hpp
+++ src/SeqLabelSettingStatistics.hpp
@@ -28,15 +28,11 @@ private:
 	std::vector<unsigned int> labels_set_size;
 	unsigned int max_temp_set_size;
 
-	// consecutive calls to markBestLabelAsPermantent without inserts
-	std::vector<unsigned int> depletion_counter; 
-	std::vector<unsigned int> depletion_sequences;
 public:
 
 	LabelSettingStatistics(unsigned int node_count) :
 		labels_set_size(node_count),
-		max_temp_set_size(0),
-		depletion_counter(node_count)
+		max_temp_set_size(0)
 	{
 		std::fill_n(data, DATA_COUNT, 0);
 	}
@@ -47,16 +43,7 @@ public:
 			if (stat == NEW_LABEL_NONDOMINATED) {
 				labels_set_size[node] = payload;
 				max_temp_set_size = std::max(max_temp_set_size, payload);
-
-				if (depletion_counter[node] > 0) {
-					depletion_sequences.push_back(depletion_counter[node]);
-					depletion_counter[node] = 0;
-				}
 			}
-			if (stat == BEST_NODE) {
-				depletion_counter[node]++;
-			}
-			
 	}
 
 	std::string toString() {
@@ -68,9 +55,6 @@ public:
 		out_stream << "  initially dominated" << ": " << dom_percent << "% (=" << data[NEW_LABEL_DOMINATED] <<")\n";
 		out_stream << "  initially non-dominated" << ": " << 100-dom_percent << "% (=" << data[NEW_LABEL_NONDOMINATED] <<")\n";
 
-		double new_best_percent = 100.0 * data[NEW_BEST_LABEL] / data[NEW_LABEL_NONDOMINATED];
-		out_stream << "    new best label" << ": " << new_best_percent << "% (=" << data[NEW_BEST_LABEL] <<")\n";
-
 		unsigned int final_total_label_count = std::accumulate(labels_set_size.begin(), labels_set_size.end(), 0);
 		unsigned int finally_dominated = data[NEW_LABEL_NONDOMINATED] - final_total_label_count;
 		double finally_dom_percent = 100.0 * finally_dominated / data[NEW_LABEL_NONDOMINATED];
@@ -81,20 +65,8 @@ public:
 		out_stream << "LabelSet sizes: " << "\n";
 		out_stream << "  avg (final)" << ": " << avg_final_set_size << "\n";
 		out_stream << "  max (final)" << ": " << max_final_set_size << "\n";
-		out_stream << "  max (temp)"  << ": " << max_temp_set_size  << "\n";
+		out_stream << "  max (temp)"  << ": " << max_temp_set_size  << "";
 
-		for (std::vector<unsigned int>::const_iterator i = depletion_counter.begin(); i != depletion_counter.end(); ++i) {
-			if (*i > 0) {
-				depletion_sequences.push_back(*i);
-			}
-		}
-		depletion_counter.clear();
-		unsigned int total_depletions = std::accumulate(depletion_sequences.begin(), depletion_sequences.end(), 0);
-		double avg_depletion_run = total_depletions / depletion_sequences.size();
-		unsigned int max_depletion_run = *std::max_element(depletion_sequences.begin(), depletion_sequences.end());
-		out_stream << "markBestLabelAsPermantent without insert into set:" << "\n";
-		out_stream << "  avg" << ": " << avg_depletion_run << "\n";
-		out_stream << "  max" << ": " << max_depletion_run << "";
 		return out_stream.str();
 
 	}
