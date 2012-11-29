0fb14ab5509b02204044d58150ca0e2135478d38
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2012-11-29 14:53:11 +0000

    Code to time large grid instances of [Machuca 2012]
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@89 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

---
+++ src/time_grid_instances2.cpp
@@ -0,0 +1,83 @@
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+#include "BiCritShortestPathAlgorithm.hpp"
+#include "GraphGenerator.hpp"
+
+#include "utility/tool/timer.h"
+#include "timing.h"
+
+void timeGrid(int num, int height, int width, bool verbose, int iterations, double p) {
+	GraphGenerator<Graph> generator;
+	double timings[iterations];
+	double label_count[iterations];
+	std::fill_n(timings, iterations, 0);
+	std::fill_n(label_count, iterations, 0);
+
+	for (int i = 0; i < iterations; i++) {
+		Graph graph;
+		generator.generateRandomGridGraphWithCostCorrleation(graph, height, width, p);
+
+		LabelSettingAlgorithm algo(graph);
+
+		utility::tool::TimeOfDayTimer timer;
+		timer.start();
+		algo.run(NodeID(0));
+		timer.stop();
+		timings[i] = timer.getTimeInSeconds();
+
+		label_count[i] = algo.size(graph.numberOfNodes()-1);
+		if (verbose) {
+			algo.printStatistics();
+		}
+	}
+	std::cout << num << " " << height << "x" << width << " " << pruned_average(timings, iterations, 0.25) << " " << pruned_average(label_count, iterations, 0) << " " << p << " # time in [s], target node label count, p " << std::endl;
+}
+
+int main(int argc, char ** args) {
+	bool verbose = false;
+	int iterations = 1;
+	double p = 0;
+
+	int c;
+	while( (c = getopt( argc, args, "c:p:v") ) != -1  ){
+		switch(c){
+		case 'c':
+			iterations = atoi(optarg);
+			break;
+		case 'p':
+			p = atof(optarg);
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		case '?':
+            std::cout << "Unrecognized option: " <<  optopt << std::endl;
+		}
+	}
+
+
+	std::cout << "# Class 1 Grid Instances of [Machuca 2012]" << std::endl;
+	// He consideres instances from 10 to 100 in increments of 10
+	// Results from his thesis:
+	//	- Nodes & edges of (100 × 100) is 10,000 and 39,600 respectively. 
+	//	- Average number of Pareto-optimal solution paths for the ten larger problems (100 × 100) 
+	//		for each correlation is 8.6, 96.2, 274.7, 435.6 and 772.3 in order of decreasing value of ρ.
+
+	std::cout << "# " << currentConfig() << std::endl;
+	timeGrid(1, 20, 20, verbose, iterations, p);
+	timeGrid(2, 40, 40, verbose, iterations, p);
+	timeGrid(3, 60, 60, verbose, iterations, p);
+	timeGrid(4, 80, 80, verbose, iterations, p);
+	timeGrid(5, 100, 100, verbose, iterations, p);
+	timeGrid(6, 120, 120, verbose, iterations, p);
+	timeGrid(7, 140, 140, verbose, iterations, p);
+	timeGrid(8, 160, 160, verbose, iterations, p);
+	timeGrid(9, 180, 180, verbose, iterations, p);
+	timeGrid(10, 200, 200, verbose, iterations, p);
+
+	return 0;
+}
+
