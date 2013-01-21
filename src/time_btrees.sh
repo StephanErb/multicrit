3fe37909cc14af7b40a4a9db626ca0866c4678d1
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-01-21 18:13:49 +0000

    Adapt the timing script accordingly.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@201 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/time_btrees.sh
+++ src/time_btrees.sh
@@ -3,37 +3,35 @@
 # Test configuration. Change these
 iter_count=10
 skew=1
-ratio=0.1
+# also, see ratio below to configure original tree size: r = n/k
 
-make time_parallel_btree.par time_btree
+make -B time_parallel_btree.par time_btree
 
-# Constants
-BULK_CONSTRUCTION=1
-BULK_INSERTION=2
 
 
 # Bulk Construction
+ratio=0
 for p in 1 2 3 4 
 do
 	out_file="../timings/btree/constr_p_"$p
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_parallel_btree.par -c $iter_count -p $p -t $BULK_CONSTRUCTION > $out_file
+	./bin/time_parallel_btree.par -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
-
 out_file="../timings/btree/constr_sequ"
 echo "Writing to sequential computation to $out_file"
-./bin/time_btree -c $iter_count -t $BULK_CONSTRUCTION > $out_file
+./bin/time_btree -c $iter_count -r $ratio -s $skew > $out_file
 
 
 # Bulk Insertion
+ratio=10 # tree is 10 times larger than the elements we try to insert
 for p in 1 2 3 4 
 do
 	out_file="../timings/btree/insert_p_"$p
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_parallel_btree.par -c $iter_count -p $p -r $ratio -s $skew -t $BULK_INSERTION > $out_file
+	./bin/time_parallel_btree.par -c $iter_count -p $p -r $ratio -s $skew  > $out_file
 done
-
 out_file="../timings/btree/insert_sequ"
 echo "Writing to sequential computation to $out_file"
-./bin/time_btree -c $iter_count -r $ratio -s $skew -t $BULK_INSERTION > $out_file
+./bin/time_btree -c $iter_count -r $ratio -s $skew  > $out_file
+
 
