0868f96facb11cc090df072f76b2ee89b215209f
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-01-22 13:47:42 +0000

    Reorganize timing scripts.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@213 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

---
+++ src/time_btree_parameter.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Test configuration. Change these
+iter_count=10
+skew=1
+ratio=100 # r = n/k
+
+# Best Sequential Node Size
+out_file="../timings/btree/insert_sequ_nodewidth"
+echo "Writing node size computation to $out_file"
+touch $out_file
+rm $out_file # clear
+for num_cachelines in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 20 32 64 128 256 512 1024
+do
+	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$num_cachelines -DLEAF_NODE_WIDTH=$num_cachelines" time_btree
+	echo -n "$num_cachelines " >> $out_file
+	./bin/time_btree -c $iter_count -r $ratio -s $skew -k 10000 >> $out_file
+done
+
+# Best Paralle Node Size
+out_file="../timings/btree/insert_p_nodewidth"
+echo "Writing node size computation to $out_file"
+touch $out_file
+rm $out_file # clear
+for num_cachelines in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 18 20 32 64 128 256 512 1024
+do
+	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$num_cachelines -DLEAF_NODE_WIDTH=$num_cachelines" time_parallel_btree.par
+	echo -n "$num_cachelines " >> $out_file
+	./bin/time_parallel_btree.par -c $iter_count -p 4 -r $ratio -s $skew -k 10000 >> $out_file
+done
--- src/time_btrees.sh
+++ src/time_btrees.sh
@@ -12,11 +12,11 @@ make -B time_parallel_btree.par time_btree
 ratio=0
 for p in 1 2 3 4 
 do
-	out_file="../timings/btree/constr_p_"$p
+	out_file="../timings/btree/insert_p_$p""_r$ratio"
 	echo "Writing to parallel computation to $out_file"
 	./bin/time_parallel_btree.par -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
-out_file="../timings/btree/constr_sequ"
+out_file="../timings/btree/insert_sequ_r$ratio"
 echo "Writing to sequential computation to $out_file"
 ./bin/time_btree -c $iter_count -r $ratio -s $skew > $out_file
 
@@ -25,37 +25,23 @@ echo "Writing to sequential computation to $out_file"
 ratio=10 # tree is 10 times larger than the elements we try to insert
 for p in 1 2 3 4 
 do
-	out_file="../timings/btree/insert_p_"$p
+	out_file="../timings/btree/insert_p_$p""_r$ratio"
 	echo "Writing to parallel computation to $out_file"
 	./bin/time_parallel_btree.par -c $iter_count -p $p -r $ratio -s $skew  > $out_file
 done
-out_file="../timings/btree/insert_sequ"
+out_file="../timings/btree/insert_sequ_r$ratio"
 echo "Writing to sequential computation to $out_file"
 ./bin/time_btree -c $iter_count -r $ratio -s $skew  > $out_file
 
 
-# Best Sequential Node Size
-ratio=10
-out_file="../timings/btree/insert_sequ_nodewidth"
-echo "Writing node size computation to $out_file"
-touch $out_file
-rm $out_file # clear
-for num_cachelines in 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 32 48 64 96 128 196 256 512 1024 
+# Bulk Insertion
+ratio=100 # tree is 100 times larger than the elements we try to insert
+for p in 1 2 3 4 
 do
-	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$num_cachelines -DLEAF_NODE_WIDTH=$num_cachelines" time_btree
-	echo -n "$num_cachelines " >> $out_file
-	./bin/time_btree -c $iter_count -r $ratio -s $skew -k 10000 >> $out_file
+	out_file="../timings/btree/insert_p_$p""_r$ratio"
+	echo "Writing to parallel computation to $out_file"
+	./bin/time_parallel_btree.par -c $iter_count -p $p -r $ratio -s $skew  > $out_file
 done
-
-# Best Paralle Node Size
-ratio=10
-out_file="../timings/btree/insert_p_nodewidth"
-echo "Writing node size computation to $out_file"
-touch $out_file
-rm $out_file # clear
-for num_cachelines in 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 32 48 64 96 128 196 256 512 1024
-do
-	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$num_cachelines -DLEAF_NODE_WIDTH=$num_cachelines" time_parallel_btree.par
-	echo -n "$num_cachelines " >> $out_file
-	./bin/time_parallel_btree.par -c $iter_count -p 4 -r $ratio -s $skew -k 10000 >> $out_file
-done
+out_file="../timings/btree/insert_sequ_r$ratio"
+echo "Writing to sequential computation to $out_file"
+./bin/time_btree -c $iter_count -r $ratio -s $skew  > $out_file
