6ce87da43ad2c639996be9213534095a62c98076
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-02-18 15:08:27 +0000

    Fix timing script.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@335 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_road2_labelsetting.sh
+++ scripts/time_road2_labelsetting.sh
@@ -15,10 +15,10 @@ done
 make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2
 out_file="../timings/road2_SequentialParetoSearch_VectorParetoQueue"
 echo "Writing to sequential computation to $out_file"
-./bin/time_road_instances2.par -d ../instances/ -g NY -c $iter_count > $out_file
+./bin/time_road_instances2 -d ../instances/ -g NY -c $iter_count > $out_file
 
 # The original, sequential algorithm
 make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm" time_road_instances2
 out_file="../timings/road2_SequentialSharedHeapLabelSettingAlgorithm_VectorSet"
 echo "Writing to sequential computation to $out_file"
-./bin/time_road_instances2.par -d ../instances/ -g NY -c $iter_count > $out_file
+./bin/time_road_instances2 -d ../instances/ -g NY -c $iter_count > $out_file
