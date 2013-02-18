39d7387b4e7d26283c0d11a1d921d5d0df64b87c
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-02-18 18:34:10 +0000

    Time different algos in different scripts.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@340 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

---
+++ scripts/time_road2_paretosearch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ../src/
+
+# Test configuration. Change these
+iter_count=1
+
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2.par
+for p in 1 2 4 8
+do
+	out_file="../timings/road2_ParallelParetoSearch_ParallelBTreeParetoQueue_p_$p"
+	echo "Writing to parallel computation to $out_file"
+	./bin/time_road_instances2.par -d ../instances/ -g NY -c $iter_count -p $p > $out_file
+done
+
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2
+out_file="../timings/road2_SequentialParetoSearch_VectorParetoQueue"
+echo "Writing to sequential computation to $out_file"
+./bin/time_road_instances2 -d ../instances/ -g NY -c $iter_count > $out_file
