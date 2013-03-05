5435bac58a82687a7a5f901f9716dda923a86003
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-03-05 15:13:40 +0000

    Apply size lesson to default BTree node sizes.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@398 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- src/datastructures/BTree_parallel.hpp
+++ src/datastructures/BTree_parallel.hpp
@@ -66,10 +66,10 @@
 
 // Width of nodes given as number of cache-lines
 #ifndef INNER_NODE_WIDTH
-#define INNER_NODE_WIDTH 8
+#define INNER_NODE_WIDTH 7
 #endif
 #ifndef LEAF_NODE_WIDTH
-#define LEAF_NODE_WIDTH 8
+#define LEAF_NODE_WIDTH 64
 #endif
 
 
