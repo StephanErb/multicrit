b87db28de8b95724a4fff302a7564dcc0cdc4e56
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-03-20 00:22:07 +0000

    Make parameter settings more specific.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@431 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_pq_btree_parameter.sh
+++ scripts/time_pq_btree_parameter.sh
@@ -4,34 +4,34 @@ cd ../src/
 # Difficult road instance ops: Size of 1 Million. Insert of size about 10000. So ratio is 100
 
 # Test configuration. Change these
-iter_count=10
+iter_count=30
 skew=1
 ratio=10 # r = n/k
 
-default_inner_node_width=8
-default_leaf_node_width=64
+default_inner_node_width=16
+default_leaf_node_width=128
 
 # Best Paralle Node Size
 out_file="../timings/btree/insert_p_nodewidth_int_inner"
 echo "Writing node size computation to $out_file"
 touch $out_file
 rm $out_file # clear
-for num_cachelines in 4 8 12 16 20 24 28 32
+for num_cachelines in 4 8 12 14 16 18 20 24 28 32
 do
 	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$num_cachelines -DLEAF_NODE_WIDTH=$default_leaf_node_width" time_pq_btree.par
 	echo -n "$num_cachelines " >> $out_file
-	./bin/time_pq_btree.par -c $iter_count -p 4 -r $ratio -s $skew -k 10000 >> $out_file
+	./bin/time_pq_btree.par -c $iter_count -p 8 -r $ratio -s $skew -k 10000 >> $out_file
 done
 
 out_file="../timings/btree/insert_p_nodewidth_int_leaf"
 echo "Writing node size computation to $out_file"
 touch $out_file
 rm $out_file # clear
-for num_cachelines in 4 8 16 32 64 128 256 512 1024
+for num_cachelines in 4 8 16 32 64 96 128 192 256 512 1024
 do
 	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$default_inner_node_width -DLEAF_NODE_WIDTH=$num_cachelines" time_pq_btree.par
 	echo -n "$num_cachelines " >> $out_file
-	./bin/time_pq_btree.par -c $iter_count -p 4 -r $ratio -s $skew -k 10000 >> $out_file
+	./bin/time_pq_btree.par -c $iter_count -p 8 -r $ratio -s $skew -k 10000 >> $out_file
 done
 
 # Best Sequential Node Size
@@ -39,7 +39,7 @@ out_file="../timings/btree/insert_sequ_nodewidth_int_inner"
 echo "Writing node size computation to $out_file"
 touch $out_file
 rm $out_file # clear
-for num_cachelines in 4 8 12 16 20 24 28 32
+for num_cachelines in 4 8 12 14 16 18 20 24 28 32
 do
 	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$num_cachelines -DLEAF_NODE_WIDTH=$default_leaf_node_width" time_pq_btree
 	echo -n "$num_cachelines " >> $out_file
@@ -50,7 +50,7 @@ out_file="../timings/btree/insert_sequ_nodewidth_int_leaf"
 echo "Writing node size computation to $out_file"
 touch $out_file
 rm $out_file # clear
-for num_cachelines in 4 8 16 32 64 128 256 512 1024
+for num_cachelines in 4 8 16 32 64 96 128 192 256 512 1024
 do
 	make -B CPPFLAGS="-DINNER_NODE_WIDTH=$default_inner_node_width -DLEAF_NODE_WIDTH=$num_cachelines" time_pq_btree
 	echo -n "$num_cachelines " >> $out_file
