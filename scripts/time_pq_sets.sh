2b0a666771c65d19f287e4a243ca5a342b36e37d
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-03-21 00:00:30 +0000

    Use taskset to setup thread pinning.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@443 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_pq_sets.sh
+++ scripts/time_pq_sets.sh
@@ -11,26 +11,25 @@ make -B time_pq_set
 # Bulk Construction
 ratio=0
 skew=1
-for p in 4
+for p in 4 8 16
 do
 	out_file="../timings/set/insert_p_$p""_r$ratio""_s$skew"
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
 
 out_file="../timings/set/insert_sequ_r$ratio""_s$skew"
 echo "Writing to sequential computation to $out_file"
 ./bin/time_pq_set -c $iter_count -r $ratio -s $skew > $out_file
 
-
 # Bulk Insertion
 ratio=10 # tree is 10 times larger than the elements we try to insert
 skew=1
-for p in 4
+for p in 4 8 16
 do
 	out_file="../timings/set/insert_p_$p""_r$ratio""_s$skew"
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
 
 out_file="../timings/set/insert_sequ_r$ratio"
@@ -40,26 +39,25 @@ echo "Writing to sequential computation to $out_file"
 # Skewed Bulk Insertion
 ratio=10 # tree is 10 times larger than the elements we try to insert
 skew=0.1
-for p in 4
+for p in 4 8 16
 do
 	out_file="../timings/set/insert_p_$p""_r$ratio""_s$skew"
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
 
 out_file="../timings/set/insert_sequ_r$ratio"
 echo "Writing to sequential computation to $out_file"
 ./bin/time_pq_set -c $iter_count -r $ratio -s $skew > $out_file
 
-
 # Bulk Insertion
 ratio=100 # tree is 100 times larger than the elements we try to insert
 skew=1
-for p in 4
+for p in 4 8 16
 do
 	out_file="../timings/set/insert_p_$p""_r$ratio""_s$skew"
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
 
 out_file="../timings/set/insert_sequ_r$ratio""_s$skew"
@@ -69,11 +67,11 @@ echo "Writing to sequential computation to $out_file"
 # Skewed Bulk Insertion
 ratio=100 # tree is 100 times larger than the elements we try to insert
 skew=0.1
-for p in 4
+for p in 4 8 16
 do
 	out_file="../timings/set/insert_p_$p""_r$ratio""_s$skew"
 	echo "Writing to parallel computation to $out_file"
-	./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_pq_mcstl_set -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
 
 out_file="../timings/set/insert_sequ_r$ratio""_s$skew"
