51e4de3c15925c08e6c5d3fd7940786a52911787
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-03-21 16:35:51 +0000

    New parameter heatmap script.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@451 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

---
+++ scripts/time_pq_btree_parameter_heatmap.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd ../src/
+
+# Difficult road instance ops: Size of 1 Million. Insert of size about 10000. So ratio is 10
+
+# Test configuration. Change these
+iter_count=100
+skew=1
+numelements=10000
+
+#################################################################
+# BRANCHING PARAMETER (LABEL)
+#################################################################
+out_file="../timings/btree/insert_nodewidth_label_leaf_p_1_heatmap"
+echo "Writing node size heatmap to $out_file"
+touch $out_file
+rm $out_file # clear
+for r in 0 10 100 1000 10000
+do
+	for k in 8 16 32 64 128 256 512 1024 2048 4096 8192 16384
+	do
+		make -B CPPFLAGS="-DUSE_GRAPH_LABEL -DLEAF_PARAMETER_K=$k" time_pq_btree.par
+		./bin/time_pq_btree.par -c $iter_count -p 1 -r $ratio -s $skew -k $numelements -h >> $out_file
+		echo -n " " >> $out_file
+	done
+	echo "\n" >> $out_file
+done
