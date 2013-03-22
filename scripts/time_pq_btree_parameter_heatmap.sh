1a99ddb1bd6e6caed9979d2515b0a325143323d1
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-03-22 12:17:17 +0000

    More detailed heatmap script.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@458 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_pq_btree_parameter_heatmap.sh
+++ scripts/time_pq_btree_parameter_heatmap.sh
@@ -9,11 +9,11 @@ numelements=10000
 #################################################################
 # BRANCHING PARAMETER (LABEL)
 #################################################################
-out_file="../timings/btree/insert_nodewidth_label_leaf_p_1_heatmap"
+out_file="../timings/btree/insert_nodewidth_label_leaf_sequ_heatmap"
 echo "Writing node size heatmap to $out_file"
 touch $out_file
 rm $out_file # clear
-for r in 0 10 100 1000 10000
+for r in 0 1 3.16 10 31.62 100 316.23 1000 3162.3 10000
 do
 	row=()
 	sum=0.0
@@ -21,6 +21,82 @@ do
 	do
 		make -B CPPFLAGS="-DUSE_GRAPH_LABEL -DLEAF_PARAMETER_K=$k" time_pq_btree
 		current=$(taskset -c 0 ./bin/time_pq_btree -c $iter_count -r $r -s $skew -k $numelements -h)
+		row+=($current)
+		sum=$(echo $sum + $current | bc)
+	done
+	echo "$r -> row average $sum"
+	for i in "${row[@]}"
+	do
+		echo "scale=4; $i / $sum" | bc | tr "\n" " " >> $out_file 
+		echo -n " " >> $out_file
+	done
+	echo "" >> $out_file
+done
+
+out_file="../timings/btree/insert_nodewidth_label_leaf_p_1_heatmap"
+echo "Writing node size heatmap to $out_file"
+touch $out_file
+rm $out_file # clear
+for r in 0 1 3.16 10 31.62 100 316.23 1000 3162.3 10000
+do
+	row=()
+	sum=0.0
+	for k in 8 16 32 64 128 256 512 1024 2048 4096 8192 16384
+	do
+		make -B CPPFLAGS="-DUSE_GRAPH_LABEL -DLEAF_PARAMETER_K=$k" time_pq_btree.par
+		current=$(taskset -c 0 ./bin/time_pq_btree.par -c $iter_count -r $r -p 1 -s $skew -k $numelements -h)
+		row+=($current)
+		sum=$(echo $sum + $current | bc)
+	done
+	echo "$r -> row average $sum"
+	for i in "${row[@]}"
+	do
+		echo "scale=4; $i / $sum" | bc | tr "\n" " " >> $out_file 
+		echo -n " " >> $out_file
+	done
+	echo "" >> $out_file
+done
+
+
+#################################################################
+# BRANCHING PARAMETER (LABEL) - detailed
+#################################################################
+out_file="../timings/btree/insert_nodewidth_label_leaf_sequ_heatmap_detailed"
+echo "Writing node size heatmap to $out_file"
+touch $out_file
+rm $out_file # clear
+for r in 0 1 3.16 10 31.62 100 316.23 1000 3162.3 10000
+do
+	row=()
+	sum=0.0
+	for k in {64..2048..64}
+	do
+		make -B CPPFLAGS="-DUSE_GRAPH_LABEL -DLEAF_PARAMETER_K=$k" time_pq_btree
+		current=$(taskset -c 0 ./bin/time_pq_btree -c $iter_count -r $r -s $skew -k $numelements -h)
+		row+=($current)
+		sum=$(echo $sum + $current | bc)
+	done
+	echo "$r -> row average $sum"
+	for i in "${row[@]}"
+	do
+		echo "scale=4; $i / $sum" | bc | tr "\n" " " >> $out_file 
+		echo -n " " >> $out_file
+	done
+	echo "" >> $out_file
+done
+
+out_file="../timings/btree/insert_nodewidth_label_leaf_p_1_heatmap_detailed"
+echo "Writing node size heatmap to $out_file"
+touch $out_file
+rm $out_file # clear
+for r in 0 1 3.16 10 31.62 100 316.23 1000 3162.3 10000
+do
+	row=()
+	sum=0.0
+	for k in {64..2048..64}
+	do
+		make -B CPPFLAGS="-DUSE_GRAPH_LABEL -DLEAF_PARAMETER_K=$k" time_pq_btree.par
+		current=$(taskset -c 0 ./bin/time_pq_btree.par -c $iter_count -r $r -p 1 -s $skew -k $numelements -h)
 		row+=($current)
 		sum=$(echo $sum + $current | bc)
 	done
