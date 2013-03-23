64fa64771ad6184a224fe78dfa5f8c60b4e63c9b
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-03-23 14:06:49 +0000

    Redirect non-error build messages.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@466 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/build_binaries.sh
+++ scripts/build_binaries.sh
@@ -6,29 +6,29 @@ make clean
 #########################################################
 # Pareto search
 #########################################################
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances1 > /dev/null
 mv ./bin/time_grid_instances1 ./bin/time_grid_instances1_paretosearch
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances1.par
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances1.par > /dev/null
 mv ./bin/time_grid_instances1.par ./bin/time_grid_instances1_paretosearch.par
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances2
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances2 > /dev/null
 mv ./bin/time_grid_instances2 ./bin/time_grid_instances2_paretosearch
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances2.par
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_grid_instances2.par > /dev/null
 mv ./bin/time_grid_instances2.par ./bin/time_grid_instances2_paretosearch.par
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances1 > /dev/null
 mv ./bin/time_road_instances1 ./bin/time_road_instances1_paretosearch
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances1.par
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances1.par > /dev/null
 mv ./bin/time_road_instances1.par ./bin/time_road_instances1_paretosearch.par
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2 > /dev/null
 mv ./bin/time_road_instances2 ./bin/time_road_instances2_paretosearch
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2.par
-mv ./bin/time_road_instances2.par ./bin/time_road_instances2_paretosearch.par
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_road_instances2.par > /dev/null
+mv ./bin/time_road_instances2.par ./bin/time_road_instances2_paretosearch.par 
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_sensor_instances
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_sensor_instances > /dev/null
 mv ./bin/time_sensor_instances ./bin/time_sensor_instances_paretosearch
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_sensor_instances.par
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=ParetoSearch" time_sensor_instances.par > /dev/null
 mv ./bin/time_sensor_instances.par ./bin/time_sensor_instances_paretosearch.par
 
 
@@ -36,30 +36,30 @@ mv ./bin/time_sensor_instances.par ./bin/time_sensor_instances_paretosearch.par
 # Classic algorithm
 #########################################################
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_grid_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_grid_instances1 > /dev/null
 mv ./bin/time_grid_instances1 ./bin/time_grid_instances1_lset_lex
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_SUM" time_grid_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_SUM" time_grid_instances1 > /dev/null
 mv ./bin/time_grid_instances1 ./bin/time_grid_instances1_lset_sum
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_MAX" time_grid_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_MAX" time_grid_instances1 > /dev/null
 mv ./bin/time_grid_instances1 ./bin/time_grid_instances1_lset_max
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_grid_instances2
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_grid_instances2 > /dev/null
 mv ./bin/time_grid_instances2 ./bin/time_grid_instances2_lset_lex
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_SUM" time_grid_instances2
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_SUM" time_grid_instances2 > /dev/null
 mv ./bin/time_grid_instances2 ./bin/time_grid_instances2_lset_sum
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_MAX" time_grid_instances2
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_MAX" time_grid_instances2 > /dev/null
 mv ./bin/time_grid_instances2 ./bin/time_grid_instances2_lset_max
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_road_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_road_instances1 > /dev/null
 mv ./bin/time_road_instances1 ./bin/time_road_instances1_lset_lex
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_SUM" time_road_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_SUM" time_road_instances1 > /dev/null
 mv ./bin/time_road_instances1 ./bin/time_road_instances1_lset_sum
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_MAX" time_road_instances1
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_MAX" time_road_instances1 > /dev/null
 mv ./bin/time_road_instances1 ./bin/time_road_instances1_lset_max
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_road_instances2
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_road_instances2 > /dev/null
 mv ./bin/time_road_instances2 ./bin/time_road_instances2_lset_lex
 
-make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_sensor_instances
+make -B CPPFLAGS="-DLABEL_SETTING_ALGORITHM=SharedHeapLabelSettingAlgorithm -DPRIORITY_LEX" time_sensor_instances > /dev/null
 mv ./bin/time_sensor_instances ./bin/time_sensor_instances_lset_lex
 
