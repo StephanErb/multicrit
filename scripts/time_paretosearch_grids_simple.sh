6ae5db14ca8278e76cb622b1b8d53b9b297ac519
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-04-15 12:26:32 +0000

    Fix copy & paste error.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@583 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_grids_simple.sh
+++ scripts/time_paretosearch_grids_simple.sh
@@ -8,10 +8,10 @@ max_costs=10
 for q in 0.8 0.4 0 -0.4 -0.8
 do
 	echo "Sequential - Computing Grid Instance "$q
-	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_vec -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_vec_q"$q"_hard"
-	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_btree -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_btree_q"$q"_hard"
+	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_vec -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_vec_q"$q
+	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_btree -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_btree_q"$q
 
 	echo "Parallel - Computing Grid Instance "$q
-	taskset -c 0-$((p-1)) ./bin/time_grid_instances2_paretosearch_ls_vec.par -p $p -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_p"$p"_vec_q"$q"_hard"
-	taskset -c 0-$((p-1)) ./bin/time_grid_instances2_paretosearch_ls_btree.par -p $p -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_p"$p"_btree_q"$q"_hard"
+	taskset -c 0-$((p-1)) ./bin/time_grid_instances2_paretosearch_ls_vec.par -p $p -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_p"$p"_vec_q"$q
+	taskset -c 0-$((p-1)) ./bin/time_grid_instances2_paretosearch_ls_btree.par -p $p -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_p"$p"_btree_q"$q
 done
