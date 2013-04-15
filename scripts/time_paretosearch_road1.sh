0fc1d64f6538af90fbd7c1dc8cc4a50a8e0d0878
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-04-15 09:28:45 +0000

    Adapt existing scripts to use both label set versions.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@576 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_road1.sh
+++ scripts/time_paretosearch_road1.sh
@@ -6,15 +6,21 @@ iter_count=30
 
 for p in 8
 do
-	out_file="../timings/road1_paretosearch_p$p"
+	out_file="../timings/road1_paretosearch_p"$p"_vec"
 	echo "Writing to parallel computation to $out_file"
-	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch.par -c $iter_count -d ../instances/ -g DC -n 1  -p $p > $out_file
-	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch.par -c $iter_count -d ../instances/ -g RI -n 10 -p $p >> $out_file
-	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch.par -c $iter_count -d ../instances/ -g NJ -n 19 -p $p >> $out_file
+	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch_ls_vec.par -c $iter_count -d ../instances/ -g DC -n 1  -p $p > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch_ls_vec.par -c $iter_count -d ../instances/ -g RI -n 10 -p $p >> $out_file
+	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch_ls_vec.par -c $iter_count -d ../instances/ -g NJ -n 19 -p $p >> $out_file
+
+	out_file="../timings/road1_paretosearch_p"$p"_btree"
+	echo "Writing to parallel computation to $out_file"
+	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch_ls_btree.par -c $iter_count -d ../instances/ -g DC -n 1  -p $p > $out_file
+	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch_ls_btree.par -c $iter_count -d ../instances/ -g RI -n 10 -p $p >> $out_file
+	taskset -c 0-$((p-1)) ./bin/time_road_instances1_paretosearch_ls_btree.par -c $iter_count -d ../instances/ -g NJ -n 19 -p $p >> $out_file
 done
 
-out_file="../timings/road1_paretosearch_sequ"
+out_file="../timings/road1_paretosearch_sequ_vec"
 echo "Writing to sequential computation to $out_file"
-taskset -c 0 ./bin/time_road_instances1_paretosearch -c $iter_count -d ../instances/ -g DC -n 1  > $out_file
-taskset -c 0 ./bin/time_road_instances1_paretosearch -c $iter_count -d ../instances/ -g RI -n 10 >> $out_file
-taskset -c 0 ./bin/time_road_instances1_paretosearch -c $iter_count -d ../instances/ -g NJ -n 19 >> $out_file
+taskset -c 0 ./bin/time_road_instances1_paretosearch_ls_vec -c $iter_count -d ../instances/ -g DC -n 1  > $out_file
+taskset -c 0 ./bin/time_road_instances1_paretosearch_ls_vec -c $iter_count -d ../instances/ -g RI -n 10 >> $out_file
+taskset -c 0 ./bin/time_road_instances1_paretosearch_ls_vec -c $iter_count -d ../instances/ -g NJ -n 19 >> $out_file
