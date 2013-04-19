a64f217942f5bf285258cc32587fbd00ebaedc20
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-04-19 09:59:26 +0000

    Disable unused grids.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@596 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_grids_hard_btree.sh
+++ scripts/time_paretosearch_grids_hard_btree.sh
@@ -5,7 +5,7 @@ iter_count=1
 max_costs=1000
 
 
-for q in 0.8 0.4 0 -0.4 -0.8
+for q in 0.8 0 -0.8
 do
 	echo "Sequential - Computing Grid Instance "$q
 	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_btree -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_btree_q"$q"_hard"
--- scripts/time_paretosearch_grids_hard_vec.sh
+++ scripts/time_paretosearch_grids_hard_vec.sh
@@ -5,7 +5,7 @@ iter_count=1
 max_costs=1000
 
 
-for q in 0.8 0.4 0 -0.4 -0.8
+for q in 0.8 0 -0.8
 do
 	echo "Sequential - Computing Grid Instance "$q
 	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_vec -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_vec_q"$q"_hard"
