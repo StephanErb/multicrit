7ffbeb1a9fde146b96a74988dc34763ec4d78782
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-04-23 11:35:10 +0000

    Start with more interesting case first.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@629 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_grids_hard_btree.sh
+++ scripts/time_paretosearch_grids_hard_btree.sh
@@ -5,7 +5,7 @@ iter_count=1
 max_costs=1000
 
 
-for q in 0.8 0 -0.8
+for q in 0 0.8 -0.8
 do
 	echo "Sequential - Computing Grid Instance "$q
 	taskset -c 0 ./bin/time_grid_instances2_paretosearch_ls_btree -v -c $iter_count -q $q -m $max_costs > "../timings/grid2_paretosearch_sequ_btree_q"$q"_hard"
