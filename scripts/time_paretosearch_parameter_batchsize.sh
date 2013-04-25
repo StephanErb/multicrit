66a527a06c9e37f7294c859102adebb19cd5ebe1
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-04-25 08:10:11 +0000

    Test slightly different parameter range.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@648 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_parameter_batchsize.sh
+++ scripts/time_paretosearch_parameter_batchsize.sh
@@ -2,9 +2,9 @@
 cd ../src/
 
 # Test configuration. Change these
-iter_count=3
-n=400
-max_cost=10
+iter_count=1
+n=300
+max_cost=1000
 q=0
 
 for p in 16 8 4 2 1 
@@ -13,7 +13,7 @@ do
 	echo "Writing batch size computation to $out_file"
 	touch $out_file
 	rm $out_file # clear
-	for b in 1 2 4 8 16 32 64 128 256 512 1024 2048
+	for b in 16 32 64 128 256 512 1024 2048 4096 8192
 	do
 		make -B CPPFLAGS="-DBATCH_SIZE=$b" time_grid_instances2.par > /dev/null
 		taskset -c 0-$((p-1)) ./bin/time_grid_instances2.par -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
