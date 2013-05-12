b342c146fd65ff8ba883857b8f5a54d7570772e1
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-05-12 07:11:14 +0000

    Fix road script. Increase PE count.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@702 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_grids_speedup.sh
+++ scripts/time_paretosearch_grids_speedup.sh
@@ -11,7 +11,7 @@ out_file="../timings/grid2_paretosearch_p_n"$n"_m"$max_cost"_q"$q
 echo "Writing speedup computation to $out_file"
 touch $out_file
 rm $out_file # clear
-for p in 32 24 20 16 12 10 8 6 4 2 1 
+for p in 64 32 24 20 16 12 10 8 6 4 2 1 
 do
 	# taskset -c 0-$((p-1))
 	./bin/time_grid_instances2_paretosearch_ls_btree.par -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
--- scripts/time_paretosearch_road_speedup.sh
+++ scripts/time_paretosearch_road_speedup.sh
@@ -9,10 +9,10 @@ out_file="../timings/road2_paretosearch_p_NY"$s
 echo "Writing speedup computation to $out_file"
 touch $out_file
 rm $out_file # clear
-for p in 32 24 20 16 12 10 8 6 4 2 1 
+for p in 64 32 24 20 16 12 10 8 6 4 2 1 
 do
 	# taskset -c 0-$((p-1))
-	./bin/time_road_instances2_paretosearch_ls_btree.par -s $s -c $iter_count -p $p -g NY >> $out_file
+	./bin/time_road_instances2_paretosearch_ls_btree.par -s $s -c $iter_count -p $p -g NY -d ../instances/ >> $out_file
 done
 
 
@@ -20,11 +20,11 @@ out_file="../timings/road2_paretosearch_sequ_NY"$s
 echo "Writing speedup computation to $out_file"
 touch $out_file
 rm $out_file # clear
-./bin/time_road_instances2_paretosearch_ls_btree -s $s -c $iter_count -g NY >> $out_file
+./bin/time_road_instances2_paretosearch_ls_btree -s $s -c $iter_count -g NY -d ../instances/ >> $out_file
 
 
 out_file="../timings/road2_lset_lex_NY"$s
 echo "Writing speedup computation to $out_file"
 touch $out_file
 rm $out_file # clear
-./bin/time_road_instances2_lset_lex -s $s -c $iter_count -g NY >> $out_file
+./bin/time_road_instances2_lset_lex -s $s -c $iter_count -g NY -d ../instances/ >> $out_file
