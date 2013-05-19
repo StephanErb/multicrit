480e30fb556cff6fd694954014a075a2ae5b016a
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-05-19 16:30:22 +0000

    Add missing sensor instances updated.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@729 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_lset_sensor.sh
+++ scripts/time_lset_sensor.sh
@@ -8,7 +8,7 @@ out_file="../timings/sensor_lset_lex_n"$n
 echo $out_file
 touch $out_file
 rm $out_file
-for d in 10 20 30
+for d in 5 10 15 20 25 30 35 40 45 50
 do
 	#taskset -c 0
 	./bin/time_sensor_instances_lset_lex -v -c $iter_count -d ../instances/sensor/ -g "n"$n"_d"$d >> $out_file
