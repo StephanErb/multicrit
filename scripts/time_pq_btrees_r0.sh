cf97fef8c4f45806072c36cb01e11d09e134caa6
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-05-20 23:38:11 +0000

    Fix PE count in construction script.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@745 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_pq_btrees_r0.sh
+++ scripts/time_pq_btrees_r0.sh
@@ -10,7 +10,7 @@ do
 	out_file="../timings/btree/insert_p_$p""_r$ratio""_s$skew"
 	echo "Writing to parallel computation to $out_file"
 	#taskset -c 0-$((p-1))
-	./bin/time_pq_btree_label_large.par -c $iter_count -p $iter_count -r $ratio -s $skew > $out_file
+	./bin/time_pq_btree_label_large.par -c $iter_count -p $p -r $ratio -s $skew > $out_file
 done
 
 out_file="../timings/btree/insert_sequ_r$ratio""_s$skew"
