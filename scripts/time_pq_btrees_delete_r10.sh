ae18e194d2593b62d7e53920657b9d7f6dbb475a
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-05-20 23:38:13 +0000

    Disable skewed deletions. They are affected by the min_problem_size.
    For simplicity (no code changes...) we just won't use them.
    
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@747 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_pq_btrees_delete_r10.sh
+++ scripts/time_pq_btrees_delete_r10.sh
@@ -30,15 +30,15 @@ iter_count=10
 for p in 1 2 4 8 16
 do
 	out_file="../timings/btree/delete_p_$p""_r$ratio""_s$skew"
-	echo "Writing to parallel computation to $out_file"
+	#echo "Writing to parallel computation to $out_file"
 	#taskset -c 0-$((p-1)) 
-	./bin/time_pq_btree_delete_label_large.par -c $iter_count -p $p -r $ratio -s $skew  > $out_file
+	#./bin/time_pq_btree_delete_label_large.par -c $iter_count -p $p -r $ratio -s $skew  > $out_file
 done
 
 out_file="../timings/btree/delete_sequ_r$ratio""_s$skew"
-echo "Writing to sequential computation to $out_file"
+#echo "Writing to sequential computation to $out_file"
 #taskset -c 0 
-./bin/time_pq_btree_delete_label_large -c $iter_count -r $ratio -s $skew  > $out_file
+#./bin/time_pq_btree_delete_label_large -c $iter_count -r $ratio -s $skew  > $out_file
 
 
 ##################
