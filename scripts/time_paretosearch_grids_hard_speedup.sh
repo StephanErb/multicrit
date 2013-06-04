2d32160f39c2b3aeb75688d4c472631ea64ab003
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-06-04 17:35:53 +0000

    For speeds, use more PEs and larger graphs.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@794 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_grids_hard_speedup.sh
+++ scripts/time_paretosearch_grids_hard_speedup.sh
@@ -8,13 +8,13 @@ max_cost=1000
 
 for q in 0
 do
-	for n in 100 200 300 400
+	for n in 100 200 300 400 500
 	do
 		out_file="../timings/grid2_paretosearch_p_n"$n"_m"$max_cost"_q"$q
 		echo "Writing speedup computation to $out_file"
 		touch $out_file
 		rm $out_file # clear
-		for p in 32 28 24 20 16 12 10 8 6 4 2 1 
+		for p in 32 28 24 20 16 14 12 10 8 6 4 2 1  
 		do
 			# taskset -c 0-$((p-1))
 			./bin/time_grid_instances2_paretosearch_ls_btree.par -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
--- scripts/time_paretosearch_road_speedup.sh
+++ scripts/time_paretosearch_road_speedup.sh
@@ -3,28 +3,32 @@ cd ../src/
 
 # Test configuration. Change these
 iter_count=3
-s=2
 
-out_file="../timings/road2_paretosearch_p_NY"$s
-echo "Writing speedup computation to $out_file"
-touch $out_file
-rm $out_file # clear
-for p in 32 28 24 20 16 12 10 8 6 4 2 1 
+for s in 2 3 
 do
-	# taskset -c 0-$((p-1))
-	./bin/time_road_instances2_paretosearch_ls_btree.par -v -s $s -c $iter_count -p $p -g NY -d ../instances/ >> $out_file
+
+	out_file="../timings/road2_paretosearch_p_NY"$s
+	echo "Writing speedup computation to $out_file"
+	touch $out_file
+	rm $out_file # clear
+	for p in 32 28 24 20 16 14 12 10 8 6 4 2 1 
+	do
+		# taskset -c 0-$((p-1))
+		./bin/time_road_instances2_paretosearch_ls_btree.par -v -s $s -c $iter_count -p $p -g NY -d ../instances/ >> $out_file
+	done
+
+
+	out_file="../timings/road2_paretosearch_sequ_NY"$s
+	echo "Writing speedup computation to $out_file"
+	touch $out_file
+	rm $out_file # clear
+	./bin/time_road_instances2_paretosearch_ls_btree -v -s $s -c $iter_count -g NY -d ../instances/ >> $out_file
+
+
+	out_file="../timings/road2_lset_lex_NY"$s
+	echo "Writing speedup computation to $out_file"
+	touch $out_file
+	rm $out_file # clear
+	./bin/time_road_instances2_lset_lex -v -s $s -c $iter_count -g NY -d ../instances/ >> $out_file
+
 done
-
-
-out_file="../timings/road2_paretosearch_sequ_NY"$s
-echo "Writing speedup computation to $out_file"
-touch $out_file
-rm $out_file # clear
-./bin/time_road_instances2_paretosearch_ls_btree -v -s $s -c $iter_count -g NY -d ../instances/ >> $out_file
-
-
-out_file="../timings/road2_lset_lex_NY"$s
-echo "Writing speedup computation to $out_file"
-touch $out_file
-rm $out_file # clear
-./bin/time_road_instances2_lset_lex -v -s $s -c $iter_count -g NY -d ../instances/ >> $out_file
