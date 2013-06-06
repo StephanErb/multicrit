1632fb0878c7c3a86c38997c5dc1274fd9b4a350
erb <erb@d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871>
2013-06-06 00:44:22 +0000

    Fix reference to new binary name.
    git-svn-id: https://algo2.iti.kit.edu/svn/erb@799 d8aefcb4-f1e2-11e1-aef8-dd4ad02cd871

--- scripts/time_paretosearch_components_grids.sh
+++ scripts/time_paretosearch_components_grids.sh
@@ -16,13 +16,13 @@ do
 		for p in 32 28 24 20 16 14 12 10 8 6 4 2 1  
 		do
 			# taskset -c 0-$((p-1))
-			./bin/time_grid_instances2_paretosearch_ls_btree_with_stats.par -s -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
+			./bin/time_grid_instances2_paretosearch_ls_btree_with_subtime.par -s -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
 		done
 
 		out_file="../timings/paretosearch_components_sequ_n"$n"_m"$max_cost"_q"$q
 		echo "Writing speedup computation to $out_file"
 		touch $out_file
 		rm $out_file # clear
-		./bin/time_grid_instances2_paretosearch_ls_btree_with_stats -s -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
+		./bin/time_grid_instances2_paretosearch_ls_btree_with_subtime -s -n $n -c $iter_count -p $p -m $max_cost -q $q >> $out_file
 	done
 done
